0cafd181ecb9d841c521469d72947aea3e716d3d
Maxwell Meyer <maxreadswell@gmail.com>
2021-03-30 10:01:03 -0700

    update js to check whether an element is a number

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,16 @@ function wordCounter(text) {
   if (text.trim().length === 0) {
     return 0;
   }
-  let wordCount = 0;
-  const wordArray = text.split(" ");
-  wordArray.forEach(function(word) {
-    wordCount++;
-  });
-  return wordCount;
-}
+  function wordCounter(text) {
+    if (text.trim().length === 0) {
+      return 0;
+    }
+    let wordCount = 0;
+    const wordArray = text.split(" ");
+    wordArray.forEach(function(element) {
+      if (!Number(element)) {
+        wordCount++;
+      }
+    });
+    return wordCount;
+  }
