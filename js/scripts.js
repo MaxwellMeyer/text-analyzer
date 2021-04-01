2077fcd44dc2003591f393d4e4ccac0edad4eb92
Maxwell Meyer <maxreadswell@gmail.com>
2021-03-31 21:47:19 -0700

    update numofoccuruncesInText function to use regex

--- js/scripts.js
+++ js/scripts.js
@@ -1,80 +1,103 @@
-// Utility Logic
+// // Utility Logic
 
-function noInputtedWord(word, text) {
-  return ((text.trim().length === 0) || (word.trim().length === 0));
-}
+// function noInputtedWord() {
+//   for (let i=0; i < arguments.length; i++) {
+//     console.log(arguments[i]);
+//     if (arguments[i].trim().length === 0) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
 
-// Business Logic
+// // Business Logic
 
-function wordCounter(text) {
-  if (text.trim().length === 0) {
-    return 0;
-  }
-  let wordCount = 0;
-  const wordArray = text.split(" ");
-  wordArray.forEach(function(element) {
-    if (!Number(element)) {
-      wordCount++;
-    }
-  });
-  return wordCount;
-}
+// function wordCounter(text) {
+//   if (noInputtedWord(text)) {
+//     return 0;
+//   }
+//   let wordCount = 0;
+//   const wordArray = text.split(" ");
+//   wordArray.forEach(function(element) {
+//     if (!Number(element)) {
+//       wordCount++;
+//     }
+//   });
+//   return wordCount;
+// }
 
+// function numberOfOccurrencesInText(word, text) {
+//   if (noInputtedWord(word, text)) 
+//     return 0;
+//   }
+//   const wordArray = text.split(" ");
+//   let wordCount = 0;
+//   wordArray.forEach(function(element) {
+//     if (element.toLowerCase().includes(word.toLowerCase())) {
+//       wordCount++;
+//     }
+//   });
+//   return wordCount;
+// }
 function numberOfOccurrencesInText(word, text) {
   if (noInputtedWord(word, text)) {
     return 0;
   }
-  const wordArray = text.split(" ");
-  let wordCount = 0;
-  wordArray.forEach(function(element) {
-    if (element.toLowerCase().includes(word.toLowerCase())) {
-      wordCount++;
-    }
-  });
-  return wordCount;
+  const regex = new RegExp(word, "gi");
+  return text.match(regex).length;
 }
+// function firstInstanceOfWord(word, text) {
+//   const textArray = text.split(" ");
+//   for (let i = 0; i < textArray.length; i++) {
+//     console.log(i);
+//     if (word === textArray[i]) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
 
-function firstInstanceOfWord(word, text) {
-  const textArray = text.split(" ");
-  for (let i = 0; i < textArray.length; i++) {
-    console.log(i);
-    if (word === textArray[i]) {
-      return i;
-    }
-  }
-  return -1;
-}
+// // UI Logic
 
-// UI Logic
+// function noInputtedWord() {
+//   for (let i=0; i < arguments.length; i++) {
+//     console.log(arguments[i]);
+//     if (arguments[i].trim().length === 0) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
 
-function boldPassage(word, text) {
-  if (noInputtedWord(word, text)) {
-    return "";
-  }
-  let htmlString = "<p>"
-  let textArray = text.split(" ");
-  textArray.forEach(function(element, index) {
-    if (element.toLowerCase().includes(word.toLowerCase())) {
-      htmlString = htmlString.concat("<b>" + element + "</b>");
-    } else {
-      htmlString = htmlString.concat(element);
-    }
-    if (index !== (textArray.length - 1)) {
-      htmlString = htmlString.concat(" ");
-    }
-  });
-  return htmlString + "</p>";
-}
+// function boldPassage(word, text) {
+//   if (noInputtedWord(word, text)) {
+//     return "";
+//   }
+//   let htmlString = "<p>"
+//   let textArray = text.split(" ");
+//   textArray.forEach(function(element, index) {
+//     if (element.toLowerCase().includes(word.toLowerCase())) {
+//       htmlString = htmlString.concat("<b>" + element + "</b>");
+//     } else {
+//       htmlString = htmlString.concat(element);
+//     }
+//     if (index !== (textArray.length - 1)) {
+//       htmlString = htmlString.concat(" ");
+//     }
+//   });
+//   return htmlString + "</p>";
+// }
 
-$(document).ready(function(){
-  $("form#word-counter").submit(function(event){
-    event.preventDefault();
-    const passage = $("#text-passage").val();
-    const word = $("#word").val();
-    const wordCount = wordCounter(passage);
-    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
-    $("#total-count").html(wordCount);
-    $("#selected-count").html(occurrencesOfWord);
-    $("#bolded-passage").html(boldPassage(word, passage));
-  });
-});
+// $(document).ready(function(){
+//   $("form#word-counter").submit(function(event){
+//     event.preventDefault();
+//     const passage = $("#text-passage").val();
+//     const word = $("#word").val();
+//     const wordCount = wordCounter(passage);
+//     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+//     $("#total-count").html(wordCount);
+//     $("#selected-count").html(occurrencesOfWord);
+//     $("#bolded-passage").html(boldPassage(word, passage));
+//   });
+// });
+
